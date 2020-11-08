b9847c895d3aa463feb4432d80353be8124f68ff
Joabe Machado <joabesonax@outlook.com>
2020-11-08 06:56:14 -0300

    base da aplicação finalizada e auth

---
+++ src/view/content/Temp/VButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn class="mx-0 font-weight-light" color="success" v-on:click="submit">
+    Executar comando
+  </v-btn>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name:"v-button",
+  methods: {
+    ...mapActions({
+      limparFlashMessage: "central_mensagens/limparFlashMessage",
+
+      submit: function() {
+        // console.log("botao apertado");
+        this.limparFlashMessage(); //① set the flash message
+        // this.$router.push({ path: "/" }); //redirect-For notification ②Show
+      }
+    })
+  }
+};
+</script>
