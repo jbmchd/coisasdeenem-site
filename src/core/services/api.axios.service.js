b4c2abe0df46866ca83fa75142c66374e1dfab97
Joabe Machado <joabesonax@outlook.com>
2020-11-10 17:27:17 -0300

    alteracoes e nova dist

--- src/core/services/api.axios.service.js
+++ src/core/services/api.axios.service.js
@@ -3,27 +3,27 @@ import store from "@/core/store";
 import AxiosService from "@jbmchd/jb-v-global/services/axios.service";
 
 const axios_interceptor_request_success = config => {
-  console.log(config);
+  // console.log(config);
   // Antes da request
   store.dispatch("axios/incrementar_processos");
   return config;
 };
 
 const axios_interceptor_request_error = error => {
-  console.log(error);
+  // console.log(error);
   // Erro na request
   store.dispatch("axios/decrementar_processos");
   return Promise.reject(error);
 };
 
 const axios_interceptor_response_success = response => {
-  console.log(response);
+  // console.log(response);
   store.dispatch("axios/decrementar_processos");
   return response;
 };
 
 const axios_interceptor_response_error = async ({ response }) => {
-  console.log(response);
+  // console.log(response);
   // Qualquer resultado diferente de 2xx
   store.dispatch("axios/decrementar_processos");
   response.data = await store.dispatch(
